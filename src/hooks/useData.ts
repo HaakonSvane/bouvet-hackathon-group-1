d8af4bfed5dbcced6cb3143baf76387a898f87ed
Thomas Næsje <thomas.naesje@bouvet.no>
2023-09-23 00:39:12 +0200

    Added fetching suggestions

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,97 +1,150 @@
 import { OpenAIClient, AzureKeyCredential, ChatMessage } from '@azure/openai';
 import { useState, useMemo, useEffect } from 'react';
 import { ChatNode, persons, PersonId } from '../types';
+import { nanoid } from 'nanoid';
 
 const client = new OpenAIClient(
-    'https://hackathon-group1-openai.openai.azure.com/',
-    new AzureKeyCredential('6fe5dccc9efd4298b9287f44100df5f5')
+  'https://hackathon-group1-openai.openai.azure.com/',
+  new AzureKeyCredential('6fe5dccc9efd4298b9287f44100df5f5')
 );
 
 export const useData = () => {
-    const [nodes, setNodes] = useState<ChatNode[]>([
+  const [nodes, setNodes] = useState<ChatNode[]>([
+    {
+      id: 'root',
+      parentId: null,
+      personId: null,
+      title: 'Jordbær',
+      text: 'Jordbær',
+    },
+  ]);
+
+  const [selectedId, setSelectedId] = useState<string>('root');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const selectedNode = useMemo(() => {
+    return nodes.find((m) => m.id === selectedId);
+  }, [nodes, selectedId]);
+
+  const selectedNodeChildren = useMemo(() => {
+    return nodes.filter((m) => m.parentId === selectedId);
+  }, [nodes, selectedId]);
+
+  const getHistory = (node: ChatNode) => {
+    const history: string[] = [];
+    let current: ChatNode | undefined = node;
+    while (current && current.parentId !== null) {
+      current.text && history.push(current.text);
+      current = nodes.find((m) => m.id === current?.parentId);
+    }
+    const promptHistory = history.map(
+      (h): ChatMessage => ({ content: h, role: 'assistant' })
+    );
+    return [
+      { content: current?.text ?? null, role: 'user' },
+      ...promptHistory,
+    ];
+  }
+
+  /** Get full text for node based on title */
+  const getText = async (node: ChatNode) => {
+    if (node.parentId === null) return;
+    if (node.personId === null) return;
+    setIsLoading(true);
+    const promptHistory = getHistory(node);
+    const { choices } = await client.getChatCompletions(
+      'exploring-for-kids',
+      [
         {
-            id: 'root',
-            parentId: null,
-            personId: null,
-            title: 'Jordbær',
-            text: 'Jordbær',
+          content: persons[node.personId].contentPrompt,
+          role: 'system',
         },
-    ]);
+        ...promptHistory,
+        {
+          content: `Tell me more about "${node.title}"`,
+          role: 'user',
+        },
+      ]
+    );
+    setNodes((m) =>
+      m.map((mm) =>
+        mm.id === node.id
+          ? { ...mm, text: choices?.[0]?.message?.content ?? null }
+          : mm
+      )
+    );
+    setSelectedId(node.id);
+    setIsLoading(false);
+  };
 
-    const [selectedId, setSelectedId] = useState<string>('root');
+  /** Get and set child nodes of current node with given user */
+  const getNextTitles = async (node: ChatNode, userId: PersonId) => {
+    if (node.parentId === null) return;
+    if (node.personId === null) return;
+    setIsLoading(true);
+    const promptHistory = getHistory(node);
+    let children: string[] = [];
+    let retries = 0;
+    while (children.length === 0 && retries++ < 3) {
+      const { choices } = await client.getChatCompletions(
+        'exploring-for-kids',
+        [
+          {
+            content: persons[node.personId].childrenPrompt,
+            role: 'system',
+          },
+          ...promptHistory,
+        ]
+      );
+      const json = choices?.[0]?.message?.content;
+      if (!json) continue;
+      try {
+        const obj = JSON.parse(json);
+        const strings: string[] = obj.content.facts;
+        if (strings.some(e => typeof e !== 'string')) continue;
+        children = strings;
+      } catch (e) {
+        console.error(e);
+      }
+    }
 
-    const selectedNode = useMemo(() => {
-        return nodes.find((m) => m.id === selectedId);
-    }, [nodes, selectedId]);
+    setNodes((m) => ([
+      ...m,
+      ...children.map((c): ChatNode => ({
+        id: nanoid(),
+        parentId: node.id,
+        personId: userId,
+        text: null,
+        title: c,
+      }))])
+    );
+    setIsLoading(false);
+  };
 
-    const selectedNodeChildren = useMemo(() => {
-        return nodes.filter((m) => m.parentId === selectedId);
-    }, [nodes, selectedId]);
+  const getPrompt = async () => {
+    const { id, created, choices, usage } = await client.getChatCompletions(
+      'exploring-for-kids',
+      [{ content: 'Test', role: 'system' }]
+    );
+    console.log({ id, created, choices, usage });
+  };
 
-    /** Get full text for node based on title */
-    const getText = async (message: ChatNode) => {
-        if (message.parentId === null) return;
-        if (message.personId === null) return;
-        const history: string[] = [];
-        let current: ChatNode | undefined = message;
-        while (current && current.parentId !== null) {
-            current.text && history.push(current.text);
-            current = nodes.find((m) => m.id === current?.parentId);
-        }
-        const promptHistory = history.map(
-            (h): ChatMessage => ({ content: h, role: 'assistant' })
-        );
-        const { choices } = await client.getChatCompletions(
-            'exploring-for-kids',
-            [
-                {
-                    content: persons[message.personId].contentPrompt,
-                    role: 'system',
-                },
-                { content: current?.text ?? null, role: 'user' },
-                ...promptHistory,
-                {
-                    content: `Tell me more about "${message.title}"`,
-                    role: 'user',
-                },
-            ]
-        );
-        setNodes((m) =>
-            m.map((mm) =>
-                mm.id === message.id
-                    ? { ...mm, text: choices?.[0]?.message?.content ?? null }
-                    : mm
-            )
-        );
-    };
+  useEffect(() => {
+    getPrompt();
+  }, []);
 
-    /** Get and set child nodes of current node with given user */
-    const getNextTitles = (message: ChatNode, userId: PersonId) => {
-        return null;
-    };
-
-    const getPrompt = async () => {
-        const { id, created, choices, usage } = await client.getChatCompletions(
-            'exploring-for-kids',
-            [{ content: 'Test', role: 'system' }]
-        );
-        console.log({ id, created, choices, usage });
-    };
-
-    useEffect(() => {
-        getPrompt();
-    }, []);
-
-    return {
-        /** Full list of nodes */
-        nodes,
-        /** Active node */
-        selectedNode,
-        /** Children of active node */
-        selectedNodeChildren,
-        /** Get the text for a given node */
-        getText,
-        /** Get the three options for the given node with the given person */
-        getNextTitles,
-    };
+  return {
+    /** Full list of nodes */
+    nodes,
+    /** Active node */
+    selectedNode,
+    /** Children of active node */
+    selectedNodeChildren,
+    /** Get the text for a given node */
+    getText,
+    /** Get the three options for the given node with the given person */
+    getNextTitles,
+    /** True if fetching data from azure */
+    isLoading,
+  };
 };
