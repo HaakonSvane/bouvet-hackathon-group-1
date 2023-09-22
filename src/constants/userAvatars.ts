1a2af31fb9fe2e0aef9368bfd7278a2f74bc2ae6
Haakon Svane <36899138+HaakonSvane@users.noreply.github.com>
2023-09-22 22:40:37 +0000

    add new images

--- src/constants/userAvatars.ts
+++ src/constants/userAvatars.ts
@@ -1,13 +1,10 @@
-import user1 from '../../public/static/user1.png';
-import user2 from '../../public/static/user2.png';
-import user3 from '../../public/static/user3.png';
-import user4 from '../../public/static/user4.png';
-import user5 from '../../public/static/user5.png';
+import dad from '../../public/static/dad.png';
+import fakeNews from '../../public/static/fakeNews.png';
+import teacher from '../../public/static/teacher.png';
+
 
 export const userAvatars = {
-    user1,
-    user2,
-    user3,
-    user4,
-    user5,
+    dad,
+    fakeNews,
+    teacher,
 };
